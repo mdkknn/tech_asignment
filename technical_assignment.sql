c23660bbedc5af2c927c48f979986ae4025bcbfc
Madoka Konno <58286233+mdkknn@users.noreply.github.com>
2022-06-15 20:13:04 +0900

    Update technical_assignment.sql

--- technical_assignment.sql
+++ technical_assignment.sql
@@ -1,14 +1,14 @@
---a. Find all orders made by consumers with the last name "Smith"
+--a. 
 SELECT * FROM Consumers WHERE last_name = 'Smith';
 
---b. Which month had the largest sales volume ever? (hint: sales volume is some aggregation of order amount)
+--b. 
 SELECT DATE_FORMAT(order_timestamp, '%y-%m') as month, SUM(order_amount_usd) as sum
 FROM Orders
 GROUP BY DATE_FORMAT(order_timestamp, '%y-%m')
 ORDER BY desc
 LIMIT 1;
 
---c. Which merchant has had the largest total sales volume?
+--c. 
 SELECT merchant_name, SUM(order_amount_usd) as total_sales FROM Orders o
 LEFT JOIN Merchants m
 IN o.merchant_id = m.merchant_id
@@ -16,20 +16,20 @@ GROUP BY merchant_name
 ORDER BY desc
 LIMIT 1;
 
---d. How many consumers have never made an order?
+--d. 
 SELECT COUNT (*) FROM Consumers c
 LEFT JOIN Orders o
 IN c.consumer_id = o.consumer_id
 WHERE o.order_id = NULL
 ;
 
---e. How many complaints have been made per consumer on average?
+--e. 
 SELECT AVG(comp.num)
 FROM (
 	SELECT COUNT(complaint_id) AS num FROM Complaints
 GROUP BY (consumer_id) as comp);
 
---f. Can you propose a simple query whose results might help us to understand if certain merchants tend to cause more complaints?
+--f. 
 SELECT merchant_name, COUNT(merchant_id) as num
 FROM Merchants m
 LEFT JOIN Orders o
